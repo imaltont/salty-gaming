fea3707c33afe82564f4ba5d26585e2b732170e7
damz2969 <sqlepytt1@gmail.com>
2015-11-11 14:39:24 +0100

    readmore button

--- scripts/dropdownart.js
+++ scripts/dropdownart.js
@@ -1,6 +1,9 @@
-function blocking(nr)
-
-	{expandText = (document.getElementById(nr).style.display == 'none') ? 'block' : 'none';	
+function blocking(nr) {
+	expandText = (document.getElementById(nr).style.display == 'none') ? 'block' : 'none';	
 	document.getElementById(nr).style.display = expandText;
-	}
+};
 
+function button(nr) {
+	innerText = (document.getElementById(nr).innerText == 'READ MORE') ? 'HIDE' : 'READ MORE';	
+	document.getElementById(nr).innerText = innerText;
+};
