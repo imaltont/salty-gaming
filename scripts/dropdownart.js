469c18c1e340c1303e8be06afee28e385fd37037
Valoss <olav_gt@hotmail.com>
2015-11-13 10:33:10 +0100

    Comments

--- scripts/dropdownart.js
+++ scripts/dropdownart.js
@@ -1,3 +1,7 @@
+//FILE NAME: dropdownart.js//
+//WRITTEN BY: Olav G. Todal//
+//WHEN: October 2015//
+//PURPOSE: For expanding textboxes on the index.html page//
 function blocking(nr) {
 	expandText = (document.getElementById(nr).style.display == 'none') ? 'block' : 'none';	
 	document.getElementById(nr).style.display = expandText;
@@ -7,3 +11,4 @@ function button(nr) {
 	innerText = (document.getElementById(nr).innerText == 'READ MORE') ? 'HIDE' : 'READ MORE';	
 	document.getElementById(nr).innerText = innerText;
 };
+
